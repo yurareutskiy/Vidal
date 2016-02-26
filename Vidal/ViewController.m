3d90d184acdafba59547b40593ddcb74dc77fb41
Anton <avscherbakov@icloud.com>
2016-02-26 19:23:25 +0300

    Started log screen

--- Vidal/ViewController.m
+++ Vidal/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.regButton.layer.borderWidth = 1.0;
+    self.regButton.layer.borderColor = [UIColor colorWithRed:148.0/255.0 green:0 blue:0 alpha:1.0].CGColor;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
