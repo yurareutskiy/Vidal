143696e7239d9b1ef6b46e6560f08e2241dc32fe
Anton <avscherbakov@icloud.com>
2016-02-26 19:01:31 +0300

    Initial commit

---
+++ Vidal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vidal
+//
+//  Created by Anton Scherbakov on 26/02/16.
+//  Copyright © 2016 StyleRU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Vidal/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Vidal
+//
+//  Created by Anton Scherbakov on 26/02/16.
+//  Copyright © 2016 StyleRU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
