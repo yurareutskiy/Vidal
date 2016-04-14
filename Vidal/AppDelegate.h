b1f25d7111ce3c465ac7fa87115a7c5169ebc7b2
yurareutskiy <yurareutskiy@gmail.com>
2016-04-14 19:13:39 +0300

    plaese,work,Anton

--- Vidal/AppDelegate.h
+++ Vidal/AppDelegate.h
@@ -12,11 +12,15 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import "OKSDK.h"
 #import "AFNetworkActivityLogger.h"
+#import <Google/CloudMessaging.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, GGLInstanceIDDelegate, GCMReceiverDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property(nonatomic, readonly, strong) NSString *registrationKey;
+@property(nonatomic, readonly, strong) NSString *messageKey;
+@property(nonatomic, readonly, strong) NSString *gcmSenderID;
+@property(nonatomic, readonly, strong) NSDictionary *registrationOptions;
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
